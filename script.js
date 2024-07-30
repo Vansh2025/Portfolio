65f88f25c19a0484cec7f60687947adc1eb3df32
Vansh2025 <vanshdeshwal9991@gmail.com>
2024-07-30 13:58:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//  typing animation
+var typing=new Typed(".typing", {
+    strings: ["", "Web Developer", "Graphic Designer","Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backdelay:100,
+    loop: true,
+});
